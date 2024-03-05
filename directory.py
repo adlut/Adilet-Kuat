26281c803c8865da88ab64d9b5957e2171662fbc
adlut <adiletkuat05@gmail.com>
2024-03-05 14:28:32 +0600

    6 lab 1

--- directory.py
+++ directory.py
@@ -1,3 +1,14 @@
 import os
-os.chdir("c:\\wert")
-print(os.getcwd())
+
+def list_directories_and_files(path):
+    if os.path.exists(path):
+        directories = [d for d in os.listdir(path) if os.path.isdir(os.path.join(path, d))]
+        files = [f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))]
+        
+        print("Directories:", directories)
+        print("Files:", files)
+        print("All Directories and Files:", os.listdir(path))
+    else:
+        print("Specified path does not exist.")
+specified_path = "c:\wert"
+list_directories_and_files(specified_path)
---
+++ function.py
@@ -0,0 +1,9 @@
+def check_grade(grade):
+    if grade>=30:
+        print("u pass")
+    else:
+        print("u have re-take")
+check_grade(31)
+check_grade(29)
+check_grade(14)
+check_grade(39)
